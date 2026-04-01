4e03f2ebe7b86e6fb17fb3f1a6da1a7f2b1ccdb5
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2026-04-02 02:42:10 +0530

    fix cora 5.5

--- server.js
+++ server.js
@@ -20,11 +20,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://golf-platform-frontend-coral.vercel.app/"],
+    origin: ["http://localhost:5173", "https://golf-platform-frontend-coral.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
+
+app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
