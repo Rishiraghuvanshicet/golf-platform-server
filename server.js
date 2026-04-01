80ff82f82973ebf595d755c12bf7cd20bf7d2cb3
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2026-04-02 02:14:55 +0530

    fix 3.5

--- server.js
+++ server.js
@@ -26,12 +26,10 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 
-
-app.options("*", cors());
-
+app.use(express.json());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
